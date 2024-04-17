b84106cdaf49e33e09edf75beeff0f9037f810e8
pedrodruviaro <pedrodruviaro@gmail.com>
2024-04-17 11:20:23 -0300

    feat: :lipstick: add google font and centered container

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,5 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'landing-page' }">Landing page</RouterLink>
-  <RouterLink :to="{ name: 'login' }">Login</RouterLink>
-
   <RouterView />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      padding: '2rem',
+      center: true
+    },
     extend: {}
   },
   plugins: []
