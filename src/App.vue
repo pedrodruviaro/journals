0157da20ac641baa27c915053d59c93f38137dc5
pedrodruviaro <pedrodruviaro@gmail.com>
2024-04-17 08:04:21 -0300

    build: add netlify redirects and deploy testing code

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,8 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <RouterLink :to="{ name: 'landing-page' }">Landing page</RouterLink>
+  <RouterLink :to="{ name: 'login' }">Login</RouterLink>
+
   <RouterView />
 </template>
