b8ef3c1785ab540a320c19f570079d48d8b7cd21
pedrodruviaro <pedrodruviaro@gmail.com>
2024-04-19 09:23:48 -0300

    fix: editor page mock fix

--- src/views/Editor/EditorEditView.vue
+++ src/views/Editor/EditorEditView.vue
@@ -3,12 +3,13 @@ import AdminLayout from '@/layouts/AdminLayout.vue'
 import EditorForm from '@/components/Editor/Form.vue'
 import EditorFormLoader from '@/components/Editor/Loader.vue'
 import { ref } from 'vue'
+import { Category } from '@/constants/category'
 
 const loading = ref(false)
 
 const title = ref('')
 const description = ref('')
-const selectedCategory = ref('')
+const selectedCategory = ref<Category>(Category.PERSONAL)
 const isPublic = ref(false)
 const content = ref('')
 
@@ -26,7 +27,6 @@ function handleSave() {
 <template>
   <AdminLayout>
     <h1 class="font-bold mb-4 text-2xl lg:text-3xl">Continue seu jornal!</h1>
-
     <EditorFormLoader :loading="loading">
       <EditorForm
         v-model:title="title"
